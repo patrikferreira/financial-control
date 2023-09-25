61c3c53d4b424bd4b67d931ab4857673beb8b198
Patrik Ferreira <patrik.andradef@gmail.com>
2023-09-25 07:15:58 -0300

    added userdto and transaction dto

---
+++ back-end/back-end/DTO/TransactionDTO.cs
@@ -0,0 +1,17 @@
+﻿using back_end.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace back_end.DTO
+{
+    public class TransactionDTO
+    {
+        [Required] public int UserId { get; set; }
+        [Required, StringLength(32)] public string Description { get; set; }
+        [Required] public decimal Amount { get; set; }
+        [StringLength(255)] public string Notes { get; set; }
+        [Required] public bool IsPaid { get; set; }
+        [Required] public TransactionType TransactionType { get; set; }
+        public int? InComeCategoryId { get; set; }
+        public int? OutComeCategoryId { get; set; }
+    }
+}
---
+++ back-end/back-end/DTO/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace back_end.DTO
+{
+    public class UserDTO
+    {
+        [Required, StringLength(32)] public string Name { get; set; }
+        [Required, StringLength(8)] public string UserName { get; set; }
+        [Required, StringLength(8)] public string Password { get; set; }
+        [Required, StringLength(120)] public string Email { get; set; }
+
+    }
+}
